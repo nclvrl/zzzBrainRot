b24017e3b158f8443bfa7919101f7718df024719
Nicole <nicolecdverial@gmail.com>
2025-02-03 19:39:17 +0800

    [init] Added integer fraction construction and simplification
    - Implements integer parameters for constructing fractions
    - Simplifies fractions using the GCD function
    - Ensures that the negative sign appears before the numerator
    
    TO DO: Fix error in unit tests

--- Fraction.py
+++ Fraction.py
@@ -1,7 +1,6 @@
 class Fraction(object):
 
     def __init__(self, numerator=0, denominator=1):
-        #TODO
         if denominator == 0:
             raise ZeroDivisionError("Denominator cannot be zero.")
         
@@ -12,12 +11,30 @@ class Fraction(object):
                 if len(fraction_parts) == 2:
                     strNum = int(fraction_parts[0])
                     strDenom = int(fraction_parts[1])
-                    
+
                     if strDenom == 0:
                         raise ZeroDivisionError("Denominator cannot be zero.")
-                    
+                        
                     self.numerator = strNum
                     self.denominator = strDenom
+            else:
+                self.numerator = 0
+                self.denominator = 1
+        elif isinstance(numerator, int):
+            self.numerator = numerator
+            self.denominator = denominator
+        else:
+            self.numerator = 0
+            self.denominator = 1
+
+        gcd_value = Fraction.gcd(self.numerator, self.denominator)
+        if gcd_value != 0:
+            self.numerator //= gcd_value
+            self.denominator //= gcd_value
+
+        if self.denominator < 0:
+            self.numerator = -self.numerator
+            self.denominator = -self.denominator
 
     def gcd(a, b):
         if a == 0 or b == 0:
